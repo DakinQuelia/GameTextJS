79d29019e8fd4b834ccff82b006e32291846b0d5
DakinQuelia <dakinquelia@gmail.com>
2025-01-04 11:30:09 +0100

    Mise à jour du code

---
+++ scripts/game/game/settings.js
@@ -0,0 +1,10 @@
+/*****************************************
+*	Jeu :: Titre de votre jeu
+*	Auteur 		: Dakin Quelia
+*	Version 	: 1.0.0. 
+*****************************************/
+settings.title = "Titre du jeu";
+settings.author = "Votre nom";
+settings.version = "0.1";
+settings.thanks = [];
+settings.warnings = "Aucun avertissement pour ce jeu.";
---
+++ scripts/game/system/items.js
@@ -0,0 +1,25 @@
+/*****************************************
+*	Jeu         : system/items.js
+*	Auteur 		: Dakin Quelia
+*	Version 	: 1.0.0. 
+*****************************************/
+class Items
+{
+    /**
+    *   Cette méthode permet de récupérer tous les objets.
+    **/
+    GetItems()
+    {
+
+    }
+
+    /**
+    *   Cette méthode permet de créer un objet.
+    **/
+    CreateItem()
+    {
+
+    }
+}
+
+export default Items;
