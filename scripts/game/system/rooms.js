09182fae8560c564af009f68287d637d5ae40520
Dakin Quelia <dakinquelia@gmail.com>
2025-01-05 22:30:41 +0100

    Ajout de la classe Système : Rooms

---
+++ scripts/game/system/rooms.js
@@ -0,0 +1,32 @@
+/*****************************************
+*	Jeu         : Items.js
+*	Auteur 		: Dakin Quelia
+*	Version 	: 1.0.0. 
+*****************************************/
+class Rooms
+{
+    /**
+	*	Constructeur
+    *
+    *   @return {void}
+	**/
+    constructor()
+    {
+
+    }
+
+    /**
+    *   Cette méthode permet de créer une nouvelle pièce.
+    *   
+    *   @param {string} name                                            Nom de la pièce
+    *   @param {object} hash                                            Informations sur la pièce { desc: "...", east:  callback, west: callback,  }µ
+    * 
+    *   @return {void}
+    **/
+    CreateRoom(name, hash)
+    {
+
+    }
+}
+
+export default new Rooms();
