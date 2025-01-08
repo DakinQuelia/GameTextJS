2d3a8009c977c1c73ce755c0066a84ffa97f5134
Dakin Quelia <dakinquelia@gmail.com>
2025-01-08 06:44:01 +0100

    Mise à jour du système de fichiers

--- scripts/lib/files.js
+++ scripts/lib/files.js
@@ -6,7 +6,27 @@
 *****************************************/
 class File
 {
-    
+    /**
+    *   Le constructeur
+    * 
+    *   @return {void}
+    **/
+    constructor()
+    {
+
+    }
+
+    /**
+    *   Cette méthode permet d'écrire dans un fichier.
+    * 
+    *   @param {string} filename                                        Nom du fichier
+    * 
+    *   @return {boolean}
+    **/
+    WriteFile(filename)
+    {
+        
+    }
 }
 
 export default new File();
