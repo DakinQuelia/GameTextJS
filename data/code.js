716369ba3e52091dd8b9ac3dc108de98d052d364
Dakin Quelia <dakinquelia@gmail.com>
2025-01-08 14:44:51 +0100

    Mise à jour du code

--- data/code.js
+++ data/code.js
@@ -75,7 +75,7 @@ button_dices.addEventListener("click", (e) =>
 
     /* Jet de dés du jeu */
     //DicesRolls("1d20", { modifier: "+1" });
-    DicesRolls("1d20", { modifier: "+2" });
+    DicesRolls("1d20", {});
 
     /* Afficher le résultat */
     DicesDisplayResults();
