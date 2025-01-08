3fd0464baf9569830f6cf0d901822a234cfc8a35
Dakin Quelia <dakinquelia@gmail.com>
2025-01-08 06:15:17 +0100

    Ajout de la classe "intro.js"

---
+++ scripts/lib/intro.js
@@ -0,0 +1,12 @@
+/*****************************************
+*	Gestion de l'introduction
+*   -------------------------
+*	Auteur 		: Dakin Quelia <dakinquelia@gmail.com>
+*	Version 	: 1.0.0. 
+*****************************************/
+class Introduction
+{   
+    
+}
+
+export default new Introduction();
