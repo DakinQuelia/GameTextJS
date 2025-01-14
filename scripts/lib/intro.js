89e9e114b74d687c7cab3e3dc59d057422971480
Dakin Quelia <dakinquelia@gmail.com>
2025-01-14 05:45:44 +0100

    Mise à jour de l'introduction

--- scripts/lib/intro.js
+++ scripts/lib/intro.js
@@ -19,29 +19,64 @@ class Introduction
     **/
     constructor()
     {
-        let span;
-        let letter;
-        let byline = document.querySelector(".intro-logo #byline");
-        let bylineText = byline.innerHTML;									  // Récupère le contenu de H2
-        let bylineArr = bylineText.split('');							     // Explose le contenu sous forme de tableau
-        
-        byline.innerHTML = '';											    // Vide le contenu courant
+        this.span;
+        this.letter;
+        this.byline = document.querySelector(".intro-logo #byline");
+        this.bylineText = this.byline.innerHTML;									                // Récupère le contenu de H2
+        this.bylineArr = this.bylineText.split('');							                        // Explose le contenu sous forme de tableau
+        this.audio = document.querySelector("#themeAudio");                                         // Audio
+        this.button_play = document.querySelector("#button_play");                                  // Bouton pour démarrer l'animation
+        this.intro_play = document.querySelector(".intro-play");                                    // Container du bouton
+        this.intro = document.querySelector(".star-wars-intro");                                    // Container de l'animation
 
-        for(let i = 0; i < bylineArr.length; i++)
-        {								
-            span = document.createElement("span");					
-            letter = document.createTextNode(bylineArr[i]);	
+        this.Reset();                                                                               // On réinitialise l'animation
 
-            if (bylineArr[i] == ' ') 
-            {												
-                byline.appendChild(letter);					
-            } 
-            else 
-            {
-                span.appendChild(letter);					
-                byline.appendChild(span); 				
+        this.button_play.addEventListener("click", () => 
+        {
+            this.intro_play.style.display = "none";
+            this.intro.style.display = "block";
+            this.byline.innerHTML = '';											                    // Vide le contenu courant
+
+            this.audio.play();
+    
+            for(let i = 0; i < this.bylineArr.length; i++)
+            {								
+                this.span = document.createElement("span");					
+                this.letter = document.createTextNode(this.bylineArr[i]);	
+    
+                if (this.bylineArr[i] == ' ') 
+                {												
+                    this.byline.appendChild(this.letter);					
+                } 
+                else 
+                {
+                    this.span.appendChild(this.letter);					
+                    this.byline.appendChild(this.span); 				
+                }
             }
-        }
+        });
+    }
+
+    /**
+    *   Cette méthode permet de réinitialiser l'animation.
+    * 
+    *   @return {void} 
+    **/
+    Reset()
+    {
+        this.audio.currentTime = 0;
+        this.intro_play.style.display = "block";
+        this.intro.style.display = "none";
+    }
+
+    /**
+    *   Cette méthode permet de lancer l'animation.
+    * 
+    *   @return {void} 
+    **/
+    Start()
+    {
+
     }
 }
 
