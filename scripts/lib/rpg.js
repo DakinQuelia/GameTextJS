41cb00b9920806b5e35b2eac516ee1eb372d6a93
Dakin Quelia <dakinquelia@gmail.com>
2025-01-16 18:30:12 +0100

    Ajout de la classe "RPG"

---
+++ scripts/lib/rpg.js
@@ -0,0 +1,31 @@
+/*****************************************
+*	Utilitaires
+*	Auteur 		: Dakin Quelia
+*	Version 	: 1.0.0. 
+*****************************************/
+import Modal from './modal.js';
+
+class RPG 
+{
+    /**
+    *   Le constructeur
+    * 
+    *   @return {void}
+    **/
+    constructor()
+    {
+        
+    }
+
+    /**
+    *   Cette méthode permet de calculer les points de statistiques.
+    * 
+    *   @return  {void}
+    **/
+    CalculateStats()
+    {
+
+    }
+}
+
+export default new RPG();
